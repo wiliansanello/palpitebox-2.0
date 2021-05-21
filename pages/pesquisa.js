37124538092e142dc085c342c2e853052580762c
Wilian Ansanello <75302006+wiliansanello@users.noreply.github.com>
2021-05-21 00:50:35 -0300

    Update pesquisa.js

--- pages/pesquisa.js
+++ pages/pesquisa.js
@@ -6,8 +6,12 @@ const Pesquisa = () => {
     Nome: '',
     Email: '',
     Whatsapp: '',
-    Critica: ''
+    Critica: '',
+     Nota: 0,
+    Indica: ''
   })
+  const notas = [0, 1, 2, 3, 4, 5]
+  const indica = ['Sim', 'Não']
   const [sucess, setSuccess] = useState(false)
   const [retorno, setRetorno] = useState({})
   const save = async () => {
@@ -47,20 +51,39 @@ const Pesquisa = () => {
         <label className='font-bold'>E-mail:</label>
         <input type='text' className='p-4 block shadow bg-blue-100 my-2 rounded' placeholder='Email' onChange={onChange} name='Email' value={form.Email} />
         <label className='font-bold'>WhatsApp:</label>
-        <input type='text' className='p-4 block shadow bg-blue-100 my-2 rounded' placeholder='WhatsApp' onChange={onChange} name='Whatsapp' value={form.Whatsapp} />
-
+        <input type='text' className='p-4 block shadow bg-blue-100 my-2 rounded' placeholder='Whatsapp' onChange={onChange} name='Whatsapp' value={form.Whatsapp} />
+        <label className='font-bold'>Sua crítica/sugestão:</label>
+        <input type='text' className='p-4 block shadow bg-blue-100 my-2 rounded' placeholder='Critica' onChange={onChange} name='Critica' value={form.Critica} />
         <p>Qual a sua nota para nosso restaurante?</p>
-        <input type='radio' id='nota_zero' name='nota' value='0' className='mx-1' />
+        <div className='flex py-6'>
+          {notas.map(nota => {
+            return (
+              <label className='block w-1/6 font-bold text-center'>
+                <input type='radio' name='Nota' value={nota} onChange={onChange} />
+                <br />{nota}
+              </label>)
+          })
+          }
+        </div>
         <p>Você nos indicaria para algum amigo?</p>
-        <input type='radio' id='resposta' name='indicacao' value='sim' className='mx-1' />
-        <div className='text-center my-8'>
+        <div className='flex py-6'>
+          {indica.map(indica => {
+            return (
+              <label className='block w-1/2 font-bold text-center'>
+                <input type='radio' name='Indica' value={indica} onChange={onChange} />
+                <br />{indica}
+              </label>)
+          })
+          }
+        </div>
+        <div className='text-center'>
           <Link href='/pesquisa'>
             <button className='bg-indigo-700 px-8 py-4 font-bold rounded-lg shadow-lg hover:shadow' onClick={save} >Enviar crítica / sugestão</button>
           </Link>
         </div>
+      
       </div>}
       {sucess && <div className='w-1/5 mx-auto my-6'>
-        cupon: {JSON.stringify(retorno)}
         <p className='mb-6 text-center pbg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3'>Obrigado por contribuir com sua sugestão ou crítica</p>
         {
           retorno.showCoupon && <div className='text-center border p-4 mb-4'>
@@ -70,7 +93,9 @@ const Pesquisa = () => {
         }
         {
           retorno.showCoupon && <div className='text-center border p-4 mb-4'>
-            <span className='font-bold'> {retorno.Promo}</span>
+            <span className='font-bold block-mb2'> {retorno.Promo}</span>
+            <br />
+            Tire um print ou foto desta tela e apresente ao garçom.      
           </div>
         }
       </div>}
