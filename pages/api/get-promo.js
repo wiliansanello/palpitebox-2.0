b6c215a7b6d807bf1d256348faff25039ed18a28
Wilian Ansanello <wiliansanello@outlook.com>
2021-08-27 10:58:26 -0300

    last changes api

--- pages/api/get-promo.js
+++ pages/api/get-promo.js
@@ -1,23 +1,37 @@
 import { GoogleSpreadsheet } from 'google-spreadsheet'
-import credentials from '../../credentials.json'
 
-const doc = new GoogleSpreadsheet('1PArFmJXjrX5vZoKjMe2MPp2sX-0U3koeAsvhpZhBO6Q')
+const doc = new GoogleSpreadsheet(process.env.SHEET_DOC_ID)
 
 export default async (req, res) => {
 
   try {
-    await doc.useServiceAccountAuth(credentials)
+    await doc.useServiceAccountAuth({
+      client_email: process.env.SHEET_CLIENT_EMAIL,
+      private_key: process.env.SHEET_PRIVATE_KEY
+    })
     await doc.loadInfo()
 
     const sheet = doc.sheetsByIndex[2]
-    await sheet.loadCells('A2:B2')
+    await sheet.loadCells('A2:B8')
 
     const mostrarPromocaoCell = sheet.getCell(1, 0)
     const textoCell = sheet.getCell(1, 1)
+    const nomeRestaurante = sheet.getCell(2, 1)
+    const endereco = sheet.getCell(3, 1)
+    const bairro = sheet.getCell(4, 1)
+    const cidade = sheet.getCell(5, 1)
+    const uf = sheet.getCell(6, 1)
+    const telefone = sheet.getCell(7, 1)
 
     res.end(JSON.stringify({
       showCoupon: mostrarPromocaoCell.value === 'VERDADEIRO',
-      message: textoCell.value
+      message: textoCell.value,
+      nomeRestaurante: nomeRestaurante.value,
+      endereco: endereco.value,
+      bairro: bairro.value,
+      cidade: cidade.value,
+      uf: uf.value,
+      telefone: telefone.value
     }))
 
   } catch (err) {
