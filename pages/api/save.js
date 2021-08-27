f4d6d6c3e47488d2768ad8fee61f20e6c208b59b
Wilian Ansanello <wiliansanello@outlook.com>
2021-08-27 10:59:50 -0300

    changed to add restaurant registration data

--- pages/api/save.js
+++ pages/api/save.js
@@ -1,20 +1,32 @@
 import { GoogleSpreadsheet } from 'google-spreadsheet'
-import credentials from '../../credentials.json'
 import moment from 'moment'
 
-const doc = new GoogleSpreadsheet('1PArFmJXjrX5vZoKjMe2MPp2sX-0U3koeAsvhpZhBO6Q')
+const doc = new GoogleSpreadsheet(process.env.SHEET_DOC_ID)
 
 const genCupom = () => {
   const code = parseInt(moment().format('YYMMDDHHmmssSSS')).toString(16).toUpperCase()
   return code.substr(0, 4) + '-' + code.substr(4, 4) + '-' + code.substr(8, 4)
 }
 
+const checkForm = (reqFields) => {
+  let countReq = 0
+  let isSave = true
+
+  for (countReq; countReq < reqFields.length; countReq++) {
+    if (reqFields[countReq] === '' || reqFields[countReq] === -1) {
+      isSave = false
+    }
+  }
+  return isSave
+}
+
 export default async (req, res) => {
 
   try {
-    console.log(JSON.parse(req.body))
-    res.end(req.body)
-    await doc.useServiceAccountAuth(credentials)
+    await doc.useServiceAccountAuth({
+      client_email: process.env.SHEET_CLIENT_EMAIL,
+      private_key: process.env.SHEET_PRIVATE_KEY
+    })
     await doc.loadInfo()
     const sheet = doc.sheetsByIndex[1]
     const data = JSON.parse(req.body)
@@ -32,22 +44,28 @@ export default async (req, res) => {
       Promo = textoCell.value
     }
 
+    const reqFields = [data.Nome, data.Email, data.Whatsapp, data.Nota, data.Indica]
+    const saveData = checkForm(reqFields)
+
     //Nome E-mail WhatsApp Cupom Promo Critica
-    await sheet.addRow({
-      Nome: data.Nome,
-      Email: data.Email,
-      Whatsapp: data.Whatsapp,
-      Cupom,
-      Promo,
-      Critica: data.Critica,
-      'Data Preenchimento': moment().format('DD/MM/YYYY, HH:mm:ss'),
-      Nota: parseInt(data.Nota),
-      Indica: data.Indica
-    })
+    if (saveData) {
+      await sheet.addRow({
+        Nome: data.Nome,
+        Email: data.Email,
+        Whatsapp: data.Whatsapp,
+        Cupom,
+        Promo,
+        Critica: data.Critica,
+        'Data Preenchimento': moment().format('DD/MM/YYYY, HH:mm:ss'),
+        Nota: parseInt(data.Nota),
+        Indica: data.Indica
+      })
+    }
     res.end(JSON.stringify({
       showCoupon: Cupom !== '',
       Cupom,
-      Promo
+      Promo,
+      saveData: saveData
     }))
   } catch (err) {
     console.log(err)
