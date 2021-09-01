6919ec8dea601a1649c2bd6ce2c3bb3d8469b0aa
Wilian Ansanello <wiliansanello@outlook.com>
2021-09-01 16:58:06 -0300

    added function fromBase64

--- pages/api/save.js
+++ pages/api/save.js
@@ -3,6 +3,11 @@ import moment from 'moment'
 
 const doc = new GoogleSpreadsheet(process.env.SHEET_DOC_ID)
 
+const fromBase64 = value => {
+  const buff = new Buffer.from(value, 'base64');
+  return buff.toString('ascii');
+}
+
 const genCupom = () => {
   const code = parseInt(moment().format('YYMMDDHHmmssSSS')).toString(16).toUpperCase()
   return code.substr(0, 4) + '-' + code.substr(4, 4) + '-' + code.substr(8, 4)
@@ -25,7 +30,7 @@ export default async (req, res) => {
   try {
     await doc.useServiceAccountAuth({
       client_email: process.env.SHEET_CLIENT_EMAIL,
-      private_key: process.env.SHEET_PRIVATE_KEY
+      private_key: fromBase64(process.env.SHEET_PRIVATE_KEY)
     })
     await doc.loadInfo()
     const sheet = doc.sheetsByIndex[1]
