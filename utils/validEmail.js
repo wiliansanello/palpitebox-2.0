6c9c520c0c0e77ef1984649dff9175b17352710e
Wilian Ansanello <wiliansanello@outlook.com>
2021-11-23 22:33:33 -0300

    function to validate email format added

---
+++ utils/validEmail.js
@@ -0,0 +1,28 @@
+const validEmail = (emailAdress) => {
+  let user = ''
+  let domain = ''
+  let isEmailValid = false
+
+  if (emailAdress.indexOf('@') !== -1) {
+
+    user = emailAdress.split('@')[0]
+    domain = emailAdress.split('@')[1]
+  }
+
+  if ((user.length >= 1) &&
+    (domain.length >= 3) &&
+    (user.search('@') == -1) &&
+    (domain.search('@') == -1) &&
+    (domain.indexOf('.') >= 1) &&
+    (domain.lastIndexOf('.') < domain.length - 1)) {
+    isEmailValid = true
+  } else {
+    isEmailValid = false
+  }
+
+  console.log(isEmailValid)
+}
+
+
+
+export default validEmail
